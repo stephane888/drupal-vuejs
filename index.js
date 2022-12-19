d47f5ffa9e78c5509ccd57f4e999a3f997690c58
kouwa stephane <kksasteph888@gmail.com>
2022-12-19 16:50:23 +0100

    Maj

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Cette approche n'est pas recommandé, car toutes les données sont importées meme si elles ne sont pas utilisées.
-// utilisé la version 2x
+// utilisé la version 3x
 export { default as drupalSession } from "./src/App/session.js";
 export { default as drupalUtilities } from "./src/App/utilities.js";
 export { default as termsTaxo } from "./src/App/jsonApi/termsTaxo.js";
